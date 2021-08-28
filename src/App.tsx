008859bf11b259f60f0d16ad5c77971f7073a199
Victor Jesus Rosario Vásquez <victorrosario.1890@gmail.com>
2021-08-28 11:27:33 -0700

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,48 @@
-import React, { useState } from 'react';
-import './App.scss';
-import { Header } from './components/header/header';
-import { Landing } from './components/landing/landing';
-import { Slider } from './components/slider/slider';
-import { Animal } from './interfaces/animals';
+import React, { useState } from "react";
+import "./App.scss";
+import { Header } from "./components/header/header";
+import { Landing } from "./components/landing/landing";
+import { Slider } from "./components/slider/slider";
+import { Animal } from "./interfaces/animals";
 
 function App() {
   const [item, setItem] = useState<number>(0);
 
   const list: Animal[] = [
     {
-      species: 'Amur Leopard',
+      species: "Amur Leopard",
       age: 2,
-      bio: 'Love snacks',
-      url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/leopard2.jpg'
+      bio: "Love snacks",
+      url: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/leopard2.jpg",
     },
     {
-      species: 'Asiatic Lion',
+      species: "Asiatic Lion",
       age: 8,
-      bio: 'Love shrimps',
-      url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/lion2.jpg'
+      bio: "Love shrimps",
+      url: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/lion2.jpg",
     },
     {
-      species: 'Siberian Tiger',
+      species: "Siberian Tiger",
       age: 9,
-      bio: 'Hate Elefants',
-      url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/tiger2.jpg'
+      bio: "Hate Elefants",
+      url: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/tiger2.jpg",
     },
     {
-      species: 'Brown Bear',
+      species: "Brown Bear",
       age: 12,
-      bio: 'Love salmon',
-      url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/bear2.jpg'
+      bio: "Love salmon",
+      url: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123024/bear2.jpg",
     },
-  ]
+  ];
 
   return (
-    <div className="App">
-      <Header></Header>
+    <div className='App'>
+      <Header />
       <Landing animals={list} item={item}></Landing>
-      <Slider onItem={(index: number) => setItem(index)} size={list.length}></Slider>
+      <Slider
+        onItem={(index: number) => setItem(index)}
+        size={list.length}
+      ></Slider>
     </div>
   );
 }
--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import './slider.scss';
+import React, { useState } from "react";
+import "./slider.scss";
 
 interface Props {
   size: number;
-  onItem: (index: number) => void
+  onItem: (index: number) => void;
 }
 
 export const Slider = ({ size, onItem }: Props) => {
   const [active, setActive] = useState<number>(0);
 
+  const handleClick = (index: number | any) => {
+    setActive(index);
+    onItem(index);
+  };
   return (
-    <div className="slider" style={{top: `calc(50% - ${size * 30}px)`}}>
-      {
-        [...Array(size)].map((_, index) => {
-          return (
-            <div
-              key={index}
-              className={`slider__item ${index === active ? 'slider__item-active' : ''}`}
-              onClick={() => {
-                setActive(index);
-                onItem(index);
-              }}
-            >
-            </div>
-          )
-        })
-      }
+    <div className='slider' style={{ top: `calc(50% - ${size * 30}px)` }}>
+      {[...Array(size)].map((_, index) => {
+        return (
+          <div
+            key={index}
+            className={`slider__item ${
+              index === active ? "slider__item-active" : ""
+            }`}
+            onClick={() => handleClick(index)}
+          ></div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
